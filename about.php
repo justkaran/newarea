d18b1654b220247d96d3ab57278fdba1f8c8aba3
justkaran <kdegani@gmail.com>
2021-07-19 20:38:36 +0200

    added new pages

---
+++ about.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <!-- Title -->
+    <title>CodeDoor</title>
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- CSS Template -->
+    <link href="static/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Skippy -->
+    <a id="skippy" class="sr-only sr-only-focusable u-skippy" href="#content">
+        <div class="container">
+            <span class="u-skiplink-text">Skip to main content</span>
+        </div>
+    </a>
+    <!-- End Skippy -->
+    <!-- Preload -->
+    <div id="loading" class="preloader">
+        <div class="spinner-border text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- End Preload -->
+    <!-- Header -->
+    <?php
+    include "header.php";
+    ?>
+    <!-- End Header -->
+    <!-- Main -->
+    <main>
+        <!-- section -->
+        <section class="section bg-gray-200">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 text-center">
+                        <h1>We are about empowerment</h1>
+                        <div class="lead">CodeDoor is a non-profit organization. We provide an easy to use AI supported web application that empowers other organisations to teach their learners how to acquire useful skills. The focus is targeted at project based learning and individual tutoring. Through our partners individual tutoring is possible via general artificial intelligence. The new skills prepare the learners for a career in a technical world.</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End section -->
+        <!-- section -->
+        <section class="section">
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-lg-6 my-3">
+                        <img src="static/img/1000x1000.jpg" title="" alt="">
+                    </div>
+                    <div class="col-lg-6 my-3 ps-lg-8">
+                        <h6 class="pb-2 text-primary">Our origins</h6>
+                        <h3>CodeDoor started as a private initiative in 2015 driven by a two-fold crisis.</h3>
+                        <p class="lead">First was the height of the refugee crisis in Europe that saw millions of people displaced, in particular youth. Second, is the continued demand for labor within software development.</p>
+                        <ul class="list-unstyled m-0 mt-5">
+                            <li class="d-flex mb-4">
+                                <div class="icon icon-sm icon-primary-light rounded-circle">
+                                    <i class="bi bi-check-all fs-5 lh-1"></i>
+                                </div>
+                                <div class="col ps-3">
+                                    <h6 class="mb-1">How it started</h6>
+                                    <p class="m-0">We launched a pilot project with just one user who learned PHP for a startup looking for a new member to join their team. </p>
+                                </div>
+                            </li>
+                            <li class="d-flex mb-4">
+                                <div class="icon icon-sm icon-warning-light rounded-circle">
+                                    <i class="bi bi-check-all fs-5 lh-1"></i>
+                                </div>
+                                <div class="col ps-3">
+                                    <h6 class="mb-1">Partners</h6>
+                                    <p class="m-0">We are grateful to be supported by leading institutions such as Aqtivator, BASF, BlackRock, BnP Paribas, Factset, GitHub, J.P. Morgan, KörberStiftung, Stifterverband, Salesforce.</p>
+                                </div>
+                            </li>
+                            <li class="d-flex mb-4">
+                                <div class="icon icon-sm icon-danger-light rounded-circle">
+                                    <i class="bi bi-check-all fs-5 lh-1"></i>
+                                </div>
+                                <div class="col ps-3">
+                                    <h6 class="mb-1">Honors</h6>
+                                    <p class="m-0">Our work has been recognized by institutions and experts in the field: Land der Ideen, KfW-Stiftung, SDW, Heinz Nixdorf Stiftung, Körber Stiftung, Deutscher Stifterverband.</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End section -->
+        <!-- section -->
+        <section class="section" id="contact">
+            <div class="container">
+                <div class="row section-heading justify-content-center">
+                    <div class="col-lg-7 col-xl-6 text-center">
+                        <h6 class="pb-3 text-primary">Many people form CodeDoor. Some are permanent employees, others work with us on a freelance basis. And a multitude of smart and generous people donate their time and knowledge to your learners by supporting them as tutors!</h6>
+                        <h3 class="h1 mb-3">Contact us!</h3>
+                        <div class="lead">We are happy to hear from you. Meet some of our team members and find out, who to best contact:</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="hover-scale">
+                            <div class="hover-scale-in rounded position-relative hover-opacity-1">
+                                <img src="static/img/1000x1000.jpg" title="" alt="">
+                                <div class="nav position-absolute bottom-0 start-0 end-0 mx-5 justify-content-center m-3 bg-white py-2 rounded-pill px-1 hover-opacity-in">
+                                        <a class="icon icon-primary mx-1 icon-sm rounded-circle" href="#">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pt-3 text-center">
+                                <h6 class="mb-2 h5">Nora Schimang, CEO</h6>
+                                <span>Contact me, if you and your organisation want to know more about CodeDoor and how we can support you. I am looking foward to hearing from you.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="hover-scale">
+                            <div class="hover-scale-in rounded position-relative hover-opacity-1">
+                                <img src="static/img/1000x1000.jpg" title="" alt="">
+                                <div class="nav position-absolute bottom-0 start-0 end-0 mx-5 justify-content-center m-3 bg-white py-2 rounded-pill px-1 hover-opacity-in">
+                                <a class="icon icon-primary mx-1 icon-sm rounded-circle" href="#">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pt-3 text-center">
+                                <h6 class="mb-2 h5">Farid Bidardel, Co-Founder</h6>
+                                <span>I love working with our wonderful funding partners. If you are interested in supporting our work as well, please get in touch!</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="hover-scale">
+                            <div class="hover-scale-in rounded position-relative hover-opacity-1">
+                                <img src="static/img/1000x1000.jpg" title="" alt="">
+                                <div class="nav position-absolute bottom-0 start-0 end-0 mx-5 justify-content-center m-3 bg-white py-2 rounded-pill px-1 hover-opacity-in">
+                                        <a class="icon icon-primary mx-1 icon-sm rounded-circle" href="#">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pt-3 text-center">
+                                <h6 class="mb-2 h5">Anna Helfrich, Senior Account Manager</h6>
+                                <span>Ask me anything about CodeDoor! If you have questions regarding our work and don´t know whom to ask - I am happy to answer them.</span>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="hover-scale">
+                            <div class="hover-scale-in rounded position-relative hover-opacity-1">
+                                <img src="static/img/1000x1000.jpg" title="" alt="">
+                                <div class="nav position-absolute bottom-0 start-0 end-0 mx-5 justify-content-center m-3 bg-white py-2 rounded-pill px-1 hover-opacity-in">
+                                        <a class="icon icon-primary mx-1 icon-sm rounded-circle" href="#">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="pt-3 text-center">
+                                <h6 class="mb-2 h5">Codolino, AI</h6>
+                                <span>I support your learners! In all modesty, I can say that I am probably the smartest (some say also the cutest) AI on the planet. </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+  <!-- End section -->
+        <!-- section -->
+        <!-- End section -->
+    </main>
+    <!-- End Main -->
+      <!-- Footer -->
+      <?php
+    include "footer.php";
+    ?>
+    <!-- End Footer -->
+    <!-- jquery -->
+    <script src="static/js/jquery-3.5.1.min.js"></script>
+    <!-- appear -->
+    <script src="static/vendor/appear/jquery.appear.js"></script>
+    <!--bootstrap-->
+    <script src="static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- countTo -->
+    <script src="static/vendor/counter/jquery.countTo.js"></script>
+    <!-- typed -->
+    <script src="static/vendor/typed/typed.js"></script>
+    <!-- owl-carousel -->
+    <script src="static/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+    <!-- magnific -->
+    <script src="static/vendor/magnific/jquery.magnific-popup.min.js"></script>
+    <!-- isotope -->
+    <script src="static/vendor/isotope/isotope.pkgd.min.js"></script>
+    <!-- svg-injector -->
+    <script src="static/vendor/svginjector/svg-injector.min.js"></script>
+    <!-- jarallax -->
+    <script src="static/vendor/jarallax/jarallax-all.js"></script>
+    <!-- count-down -->
+    <script src="static/vendor/count-down/jquery.countdown.min.js"></script>
+    <!-- working form -->
+    <script src="static/vendor/mail/js/form.min.js"></script>
+    <script src="static/vendor/mail/js/script.js"></script>
+    <!-- Theme Js -->
+    <script src="static/js/custom.js"></script>
+</body>
+<!-- end body -->
+
+</html>
