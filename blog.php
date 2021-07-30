20ceaf2703fe22ed94b55c2f16d85f4f644ec806
justkaran <kdegani@gmail.com>
2021-07-30 13:49:45 +0200

    new blog etnry

--- blog.php
+++ blog.php
@@ -44,8 +44,8 @@
                     <div class="col-lg-8">
                         <h1 class="display-5 text-white m-0 text-center">Hello World, Welcome to our Blog!</h1>
                         <ul class="nav mt-4 justify-content-center">
-                            <li class="h6 mx-2 text-white font-weight-normal"><i class="fas fa-user me-2"></i> CodeDoor Team</li>
-                            <li class="h6 mx-2 text-white font-weight-normal"><i class="far fa-clock me-2"></i> 28th July, 2021</li>
+                            <li class="h6 mx-2 text-white font-weight-normal"><i class="fas fa-user me-2"></i>Latest Entry by: Karan</li>
+                            <li class="h6 mx-2 text-white font-weight-normal"><i class="far fa-clock me-2"></i> Latest Entry: July 30, 2021</li>
                         </ul>
                     </div>
                 </div>
@@ -57,16 +57,87 @@
             <div class="row justify-content-center">
                 <div class="col-lg-9">
                     <article class="card card-body mt-n10">
-                        <h5>We have a new design</h5>
-                        <p>Welcome to our new site! We are excited to announce that we have a new design.
-                        Have fun exploring the page. We are looking forward to hearing your thoughts.</p>
+                        <h5>A sense of where you are</h5>
+                        <article> This is based on an article published in the yearly report of <a href="https://enpact.org">enpact.org</a>. One of the greatest startup supporting organizations we partner with. </p>
+                            <p>
+                                Who does not feel dizzy when looking at all the events that have happened in 2020? It looked like the spinning top that is increasing its speed while an international rock band sings: “The world clearly has gone mad.”
+                                Until 2020, we haven't seriously thought about the craziness of the world. We always thought that the world is a mad place. But 2020 puts everything into a new perspective. So we decided to look at the madness of the world more closely.
+                            </p>
+                            <p>
+                                Social problems appear like moving targets. To solve these problems, old static ways don't work anymore. Most variables in the equations are not constant anymore. We realized that we have to find new dynamically adapting ways.
+                                Let us take you on a short journey-of-thought of how we at CodeDoor have approached this context of accelerated changes of variables values.
+                            </p>
+                                Because the world seems to be unpredictable, you cannot apply conventional methods, right? We had to come up with new ways. As every good engineer does, we removed assumptions and conventions about how things have been done in the past. We stripped things down to their fundamentals and looked at what variables will most likely have the highest velocity in changing values.
+                                To identify the variables we first asked, what questions should we ask. The following are some questions we tried to answer. If you want, don’t look at the answers, write your own answers down first, then look at our answers.
+                                What did you come up with? We are happy to read yours.
+                            <p>
+                            <h4>
+                                First Question: Is the world actually getting crazier?
+                            </h4>
+                            <p>
+                                Our answer: We do not need a detailed investigation of all events ever happened to compare and measure their craziness. The trick is that if an event feels unprecedented, we will most likely perceive it as crazy. As everyone knows, events that appear unprecedented in their scope pop up everywhere. So the world is indeed getting crazier. The context the world gets itself into, turns about to be even worse than crazy: When un-craziness and crazy are mixed, un-craziness does not work as a counterbalance anymore and the result is craziness. Or put differently, although a lot of people work for the betterment of our plant, the few ones that don’t mess up so much that the world barely breathes.
+                            </p>
+                            <h4>
+                                Second Question: With all the technological advances, why do these crazy events occur?
+                            </h4>
+                            <p>
+                                Our answer: If you look at it from a human related analogy, the world resembles a teenager in an identity crisis. Irrationality reaches their highest point of intersection with physical possibilities. The results are waves hitting a teenager’s mind in crisis.
+                            </p>
+                            <h4>
+                                Third Question: How come that tiny events suddenly become so world embracingly powerful?
+                            </h4>
+                            <p>
+                            Our answer: In short, we believe it’s a combination of globalization and nonlinear dynamics. What on earth is that you might ask? It becomes obvious with events where the impact increases over time. The infinitesimal transition of a virus from patient 0 to a human might not have a massive impact on the human at the very moment of transition. But as Lorenz found out with the Butterfly Effect, over time some events seem to be insignificant for a short time. Later they turn out to be world-changing events.
+                            </p>
+                            <h4>
+                                Fourth Question: Does it matter that the world is going mad?
+                            </h4>
+                            <p>
+                                Our Answer: It makes a big difference to us. It means that the phrase "the world has gone mad" is not only a figure of speech to express one's displeasure. It's not just a phrase to express disbelief. It makes us think about how to avoid and to change crazy developments. The world should not only not be crazy. It should be like Jürgen Klopp "the normal one." 
+                            </p>
+                            <h4>
+                                Fifth Question: Why should I care if the world is mad?
+                            </h4>
+                            <p>
+                            Our Answer: Because these waves have the potential to attack our civilization.  For a stable civilization, we need to trust it. The world can face catastrophes, but only as long as people have a strong belief in their civilization. We need to be confident in their ability to deal with threats. You could argue that our civilization or society is far too stable and that technology is too sophisticated for us to lose confidence in our systems. Most Romans thought so too.
+                            </p>
+                            <h4>
+                            Sixth Question: What are the greatest enemies of our civilization?
+                            </h4>
+                            <p>
+                            Our Answer: One of civilization’s chief enemies is fear. Fear of the things that could make life no longer worthwhile, such as illness, war or poverty. The second major enemy is despair and meaninglessness. When you lose hope of a life worth living, you question your identity and the perceived realities of your surroundings. That opens the door to demagogues. Almost everyone who touches the masses with targeted emotional impulses then has an easy job of displacing these masses. The alternative is accepted with gratitude, all is better than none.
+                            </p>
+                            <h4>
+                            Seventh Question: How can individuals be empowered to lose or to prevent fear?
+                            </h4>
+                            <p>
+                            Our Answer: The situation to which the world finds itself is not without hope. The key is to learn to be aware of the events and processes that contribute to their social and material well-being. However, no one can be alert in any aspect of life. But they may be within their own area of competence. It doesn’t matter what you are involved in, whether it’s economics, education, history, social sciences, philosophy, and sports, or whatever. You can reflect on and take action on events in your field of expertise.  
+                            </p>
+                            <h4>
+                            Eighths Question: What is the enemy of empowerment?
+                            </h4>
+                            <p>
+                            Our Answer: Certainly, people need access to resources to take action. But there is also something counterintuitive on the individual level, which has nothing to do with resources. People should not wait to be prepared to take action. Most people expect the feeling to appear. But psychiatrists will agree that motivation does not come first. Action does. If people take action, even if they don’t feel like it, this action produces energy, and pockets of motivation follow. It may start with exploring new approaches with a few simple steps that can grow into complexity. It’s like a startup idea. Version one of a successful product is usually quite simple and iterates fairly quickly into a sophisticated solution
+                            </p>
+                            <h4>
+                            Ninth Question: Ok, let’s say people are empowered. How do people find out what they should work on?
+                            </h4>
+                            <p>
+                            Our Answer: They should discover who they are. To do this, they need to have the right technical and social environment that gives them access to tools that enable them to identify the skills they want to inherently improve and use to act. This leads to a life of contribution in which each human being can be empowered to contribute her individual skills to her social and material well-being.
+                            </p>
+                            <h4>
+                            Tenth Question: Why do things that matter always take so long to evolve?
+                            </h4>
+                            <p>
+                            Our Answer:  When people start a journey they need to be attentive to long-term needs as well. Most of the things we want and that are significant can only be achieved over the long term. And because they take time, we lack a prompt reward for our actions. And although perseverance is often the hallmark of a successful enterprise, we often act in the short term and undervalue what we are able to do in the long term.
+                            </p>
                         <div class="border-top pt-4 mt-3">
                             <div class="row align-items-center">
                                 <div class="col-md-6">
                                     <div class="media align-items-center">
                                         <img class="avatar rounded-circle" src="static/img/cddr-logo.png" title="" alt="">
                                         <div class="media-body ps-3">
-                                            <h6 class="mb-1">Your CodeDoor Team</h6>
+                                            <h6 class="mb-1">Karan from the CodeDoor Team</h6>
                                         </div>
                                     </div>
                                 </div>
@@ -90,6 +161,44 @@
                     </div>
                 </div>
             </div>
+            <div class="container position-relative bg-white mt-4">
+            <div class="row justify-content-center">
+                <div class="col-lg-9">
+                    <article class="card card-body">
+                        <h5>We have a new design</h5>
+                        <p>Welcome to our new site! We are excited to announce that we have a new design.
+                        Have fun exploring the page. We are looking forward to hearing your thoughts.</p>
+                        <div class="border-top pt-4 mt-3">
+                            <div class="row align-items-center">
+                                <div class="col-md-6">
+                                    <div class="media align-items-center">
+                                        <img class="avatar rounded-circle" src="static/img/cddr-logo.png" title="" alt="">
+                                        <div class="media-body ps-3">
+                                            <h6 class="mb-1">July 28th 2021, Your CodeDoor Team</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="nav justify-content-end">
+                                        <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://www.facebook.com/sharer/sharer.php?u=https://codedoor.com" target="_blank">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </a>
+                                        <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://twitter.com/intent/tweet" target="_blank" >
+                                            <i class="fab fa-twitter"></i>
+                                        </a>
+                                        <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://www.linkedin.com/sharing/share-offsite/?url={https://codedoor.com}" target="_blank">
+                                            <i class="fab fa-linkedin-in"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
         <!-- end section -->
         <!-- section -->
@@ -168,10 +277,10 @@
     </main>
     <!-- End Main -->
    <!-- Footer -->
-   <?php
-   include "footer.php";
-   ?>
-   <!-- End Footer -->
+      <?php
+      include "footer.php";
+      ?>
+      <!-- End Footer -->
 
     <!-- jquery -->
     <script src="static/js/jquery-3.5.1.min.js"></script>
