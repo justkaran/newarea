b8e58f2c5a9463cf8586a1bc8b544cbdde169d1c
Tobias Lang <t.lang@i6m.de>
2021-09-21 16:24:26 +0200

    add 2 new blog entries

---
+++ blog-admin-onboarding.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <!-- Title -->
+    <title>CodeDoor - Project Based Learning</title>
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- CSS Template -->
+    <link href="static/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<!-- Skippy -->
+<a id="skippy" class="sr-only sr-only-focusable u-skippy" href="#content">
+    <div class="container">
+        <span class="u-skiplink-text">Skip to main content</span>
+    </div>
+</a>
+<!-- End Skippy -->
+<!-- Preload -->
+<div id="loading" class="preloader">
+    <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
+<!-- End Preload -->
+<!-- Header -->
+<?php
+include "header.php";
+?>
+<!-- End Header -->
+<!-- Main -->
+<main>
+    <!-- section -->
+    <section class="parallax effect-section" style="background-color:#0D4963">
+        <div class="mask bg-black opacity-7"></div>
+        <div class="container position-relative">
+            <div class="row min-vh-50 align-items-center justify-content-center pt-8 pb-12">
+                <div class="col-lg-8">
+                    <h1 class="display-5 text-white m-0 text-center">How to start CodeDoor as an administrator</h1>
+                    <ul class="nav mt-4 justify-content-center">
+                        <li class="h6 mx-2 text-white font-weight-normal"><i class="fas fa-user me-2"></i>Latest Entry by: CodeDoor Team</li>
+                        <li class="h6 mx-2 text-white font-weight-normal"><i class="far fa-clock me-2"></i> Latest Entry: September 21, 2021</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End section -->
+    <!-- section -->
+    <div class="container position-relative bg-white mt-4 mb-12">
+        <div class="row justify-content-center">
+            <div class="col-lg-9">
+                <article class="card card-body">
+                    <h5>How to start CodeDoor as an administrator</h5>
+                    <p>We are very much looking forward to welcoming you to CodeDoor! In case of questions you can contact us via <a href="mailto:hello@codedoor.org">hello@codedoor.org</a>. Please do not hesitate to contact us - we would love to help you!</p>
+                    <p>Before you can start using CodeDoor, you need to receive an email invite from with your personal registration invite.</p>
+                    <p>If this first message does not reach you, please start by checking your spam filter. If you do not find it there, please reach out to your CodeDoor contact and we will check what happened to your message!</p>
+                    <p>This is the first message you receive:</p>
+
+                    <img src="static/img/screenshots/Mail_register.png">
+                    <br/>
+                    <p>Use the "Register now"-button to register:</p>
+
+                    <p>Register yourself by using your first and last name. Please choose your own password:</p>
+                    <img src="static/img/screenshots/Registration.jpeg">
+
+                    <p>After registration, you will reach your personal dashboard on CodeDoor.</p>
+
+                    <div class="border-top pt-4 mt-3">
+                        <div class="row align-items-center">
+                            <div class="col-md-6">
+                                <div class="media align-items-center">
+                                    <img class="avatar rounded-circle" src="static/img/cddr-logo.png" title="" alt="">
+                                    <div class="media-body ps-3">
+                                        <h6 class="mb-1">September 21th 2021, Your CodeDoor Team</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="nav justify-content-end">
+                                    <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://www.facebook.com/sharer/sharer.php?u=https://codedoor.com" target="_blank">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                    <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://twitter.com/intent/tweet" target="_blank" >
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                    <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://www.linkedin.com/sharing/share-offsite/?url={https://codedoor.com}" target="_blank">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+
+</main>
+<!-- End Main -->
+<!-- Footer -->
+<?php
+include "footer.php";
+?>
+<!-- End Footer -->
+
+<!-- jquery -->
+<script src="static/js/jquery-3.5.1.min.js"></script>
+<!-- appear -->
+<script src="static/vendor/appear/jquery.appear.js"></script>
+<!--bootstrap-->
+<script src="static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- countTo -->
+<script src="static/vendor/counter/jquery.countTo.js"></script>
+<!-- typed -->
+<script src="static/vendor/typed/typed.js"></script>
+<!-- owl-carousel -->
+<script src="static/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+<!-- magnific -->
+<script src="static/vendor/magnific/jquery.magnific-popup.min.js"></script>
+<!-- isotope -->
+<script src="static/vendor/isotope/isotope.pkgd.min.js"></script>
+<!-- svg-injector -->
+<script src="static/vendor/svginjector/svg-injector.min.js"></script>
+<!-- jarallax -->
+<script src="static/vendor/jarallax/jarallax-all.js"></script>
+<!-- count-down -->
+<script src="static/vendor/count-down/jquery.countdown.min.js"></script>
+<!-- working form -->
+<script src="static/vendor/mail/js/form.min.js"></script>
+<script src="static/vendor/mail/js/script.js"></script>
+<!-- Theme Js -->
+<script src="static/js/custom.js"></script>
+</body>
+<!-- end body -->
+
+</html>
