127599da0f3e2791aecebbd6a35d5163ea65ae3d
Tobias Lang <t.lang@i6m.de>
2021-12-02 12:51:18 +0100

    blog edesign

---
+++ blog-edesign.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <!-- Title -->
+    <title>CodeDoor - Project Based Learning</title>
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- CSS Template -->
+    <link href="static/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<!-- Skippy -->
+<a id="skippy" class="sr-only sr-only-focusable u-skippy" href="#content">
+    <div class="container">
+        <span class="u-skiplink-text">Skip to main content</span>
+    </div>
+</a>
+<!-- End Skippy -->
+<!-- Preload -->
+<div id="loading" class="preloader">
+    <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
+<!-- End Preload -->
+<!-- Header -->
+<?php
+include "header.php";
+?>
+<!-- End Header -->
+<!-- Main -->
+<main>
+    <!-- section -->
+    <section class="parallax effect-section" style="background-color:#0D4963">
+        <div class="container position-relative">
+            <div class="row min-vh-50 align-items-center justify-content-center pt-8 pb-12">
+                <div class="col-lg-8">
+                    <h1 class="display-5 text-white m-0 text-center">E-Design Handbuch</h1>
+                    <ul class="nav mt-4 justify-content-center">
+                        <li class="h6 mx-2 text-white font-weight-normal"><i class="fas fa-user me-2"></i>Latest Entry by: CodeDoor Team</li>
+                        <li class="h6 mx-2 text-white font-weight-normal"><i class="far fa-clock me-2"></i> Latest Entry: December 2, 2021</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End section -->
+    <!-- section -->
+    <div class="container position-relative bg-white mt-4 mb-12">
+        <div class="row justify-content-center">
+            <div class="col-lg-9">
+                <article class="card card-body">
+                    <h5>European Union project E-Design</h5>
+                    <p>Since 2019, CodeDoor has been working on the European Union project E-Design (European Digital Education for Social Inclusion and Global Neighbourhood). In an increasingly global and digital world, ICT skills are becoming more and more important and are required in many professions. The aim of the E-Design project was to promote social inclusion and to aquire or strengthen digital skills in order to combat social inequalities in disadvantaged groups.</p>
+                    <p>It aimed to strengthen the structures of voluntary work in schools and communities to provide digital education free of charge and accessible to all. In addition to the acquisition of skills, there was also the opportunity to socialise in the community, which promotes social inclusion.</p>
+                    <p>During the project, training hotspots were set up in Italy, Greece, Portugal and Germany. In these, courses were held on various topics such as 3D printing, programming or microcontrollers.</p>
+                    <p>International exchange between the individual hotspots was always a priority. In addition to the courses, work was also done on learning videos, course materials and a platform for permanent exchange and learning.</p>
+                    <p>From the respective courses and the associated experiences, a handbook was developed that is now freely available to all interested parties. It contains course contents, instructions for teachers and preparation materials.
+                    </p>
+                    <p><a href="static/E-DESIGN-Handbuch.zip" download="E-DESIGN-Handbuch.zip">Download Handbuch (DE)</a>
+                    </p>
+
+
+                    <img src="static/img/blog/edesign.jpg">
+                    <br/>
+
+                    <div class="border-top pt-4 mt-3">
+                        <div class="row align-items-center">
+                            <div class="col-md-6">
+                                <div class="media align-items-center">
+                                    <img class="avatar rounded-circle" src="static/img/cddr-logo.png" title="" alt="">
+                                    <div class="media-body ps-3">
+                                        <h6 class="mb-1">December 2th 2021, Your CodeDoor Team</h6>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="nav justify-content-end">
+                                    <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://www.facebook.com/sharer/sharer.php?u=https://codedoor.com" target="_blank">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                    <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://twitter.com/intent/tweet" target="_blank" >
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                    <a class="icon icon-sm rounded-circle icon-primary ms-2" href="https://www.linkedin.com/sharing/share-offsite/?url={https://codedoor.com}" target="_blank">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </div>
+
+</main>
+<!-- End Main -->
+<!-- Footer -->
+<?php
+include "footer.php";
+?>
+<!-- End Footer -->
+
+<!-- jquery -->
+<script src="static/js/jquery-3.5.1.min.js"></script>
+<!-- appear -->
+<script src="static/vendor/appear/jquery.appear.js"></script>
+<!--bootstrap-->
+<script src="static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- countTo -->
+<script src="static/vendor/counter/jquery.countTo.js"></script>
+<!-- typed -->
+<script src="static/vendor/typed/typed.js"></script>
+<!-- owl-carousel -->
+<script src="static/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+<!-- magnific -->
+<script src="static/vendor/magnific/jquery.magnific-popup.min.js"></script>
+<!-- isotope -->
+<script src="static/vendor/isotope/isotope.pkgd.min.js"></script>
+<!-- svg-injector -->
+<script src="static/vendor/svginjector/svg-injector.min.js"></script>
+<!-- jarallax -->
+<script src="static/vendor/jarallax/jarallax-all.js"></script>
+<!-- count-down -->
+<script src="static/vendor/count-down/jquery.countdown.min.js"></script>
+<!-- working form -->
+<script src="static/vendor/mail/js/form.min.js"></script>
+<script src="static/vendor/mail/js/script.js"></script>
+<!-- Theme Js -->
+<script src="static/js/custom.js"></script>
+</body>
+<!-- end body -->
+
+</html>
--- blog.php
+++ blog.php
@@ -15,132 +15,142 @@
 </head>
 
 <body>
-    <!-- Skippy -->
-    <a id="skippy" class="sr-only sr-only-focusable u-skippy" href="#content">
-        <div class="container">
-            <span class="u-skiplink-text">Skip to main content</span>
-        </div>
-    </a>
-    <!-- End Skippy -->
-    <!-- Preload -->
-    <div id="loading" class="preloader">
-        <div class="spinner-border text-primary" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
+<!-- Skippy -->
+<a id="skippy" class="sr-only sr-only-focusable u-skippy" href="#content">
+    <div class="container">
+        <span class="u-skiplink-text">Skip to main content</span>
     </div>
-    <!-- End Preload -->
-         <!-- Header -->
-         <?php
-         include "header.php";
-         ?>
-         <!-- End Header -->
-    <!-- Main -->
-    <main>
-        <!-- section -->
-        <section class="parallax effect-section" style="background-color:#0D4963">
-            <div class="container position-relative">
-                <div class="row min-vh-50 align-items-center justify-content-center pt-8 pb-0">
-                    <div class="col-lg-8">
-                        <h1 class="display-5 text-white m-0 text-center">Our blog</h1>
-                    </div>
+</a>
+<!-- End Skippy -->
+<!-- Preload -->
+<div id="loading" class="preloader">
+    <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div>
+<!-- End Preload -->
+<!-- Header -->
+<?php
+include "header.php";
+?>
+<!-- End Header -->
+<!-- Main -->
+<main>
+    <!-- section -->
+    <section class="parallax effect-section" style="background-color:#0D4963">
+        <div class="container position-relative">
+            <div class="row min-vh-50 align-items-center justify-content-center pt-8 pb-0">
+                <div class="col-lg-8">
+                    <h1 class="display-5 text-white m-0 text-center">Our blog</h1>
                 </div>
             </div>
-        </section>
-        <!-- End section -->
-        <!-- section -->
+        </div>
+    </section>
+    <!-- End section -->
+    <!-- section -->
 
-        <section class="section bg-gray-100">
-            <div class="container">
-                <div class="row g-3 g-lg-4">
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm-hover">
-                            <a href="blog-interviews.php"><img class="card-img-top" src="static/img/blog/CodeDoor_StudentInnen.jpg" title="" alt="">
-                                <div class="card-body pt-0">
-                                    <div class="text-muted mb-4 mt-n3"><span class="bg-primary text-white py-1 px-3 d-inline-block rounded-pill small"><i class="far fa-clock me-2"></i>November 8th 2021</span></div>
-                                    <h5>Meet our awesome alumni</h5>
-                                    <p class="pt-3 m-0">We asked CodeDoor alumni to share their experiences and give tips to current learners...</p>
-                                </div></a>
-                        </div>
+    <section class="section bg-gray-100">
+        <div class="container">
+            <div class="row g-3 g-lg-4">
+                <div class="col-lg-4">
+                    <div class="card shadow-sm-hover">
+                        <a href="blog-edesign.php"><img class="card-img-top" src="static/img/blog/edesign.jpg" title="" alt="">
+                            <div class="card-body pt-0">
+                                <div class="text-muted mb-4 mt-n3"><span class="bg-primary text-white py-1 px-3 d-inline-block rounded-pill small"><i class="far fa-clock me-2"></i>November 8th 2021</span></div>
+                                <h5>E-DESIGN project</h5>
+                                <p class="pt-3 m-0">CodeDoor is part of the E-DESIGN project...</p>
+                            </div></a>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm-hover">
-                            <a href="blog-espinho.php"><img class="card-img-top" src="static/img/blog/code4sp_Espinho.jpg" title="" alt="">
+                </div>
+                <div class="col-lg-4">
+                    <div class="card shadow-sm-hover">
+                        <a href="blog-interviews.php"><img class="card-img-top" src="static/img/blog/CodeDoor_StudentInnen.jpg" title="" alt="">
+                            <div class="card-body pt-0">
+                                <div class="text-muted mb-4 mt-n3"><span class="bg-primary text-white py-1 px-3 d-inline-block rounded-pill small"><i class="far fa-clock me-2"></i>November 8th 2021</span></div>
+                                <h5>Meet our awesome alumni</h5>
+                                <p class="pt-3 m-0">We asked CodeDoor alumni to share their experiences and give tips to current learners...</p>
+                            </div></a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card shadow-sm-hover">
+                        <a href="blog-espinho.php"><img class="card-img-top" src="static/img/blog/code4sp_Espinho.jpg" title="" alt="">
                             <div class="card-body pt-0">
                                 <div class="text-muted mb-4 mt-n3"><span class="bg-primary text-white py-1 px-3 d-inline-block rounded-pill small"><i class="far fa-clock me-2"></i>November 2th 2021</span></div>
                                 <h5>code4sp Espinho</h5>
                                 <p class="pt-3 m-0">The EU-funded project code4sp has been running since January 2021, but...</p>
                             </div></a>
-                        </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm-hover">
-                            <a href="blog-save-the-oceans.php"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/07/27/19/47/turtle-863336_1280.jpg" title="" alt="">
+                </div>
+                <div class="col-lg-4">
+                    <div class="card shadow-sm-hover">
+                        <a href="blog-save-the-oceans.php"><img class="card-img-top" src="https://cdn.pixabay.com/photo/2015/07/27/19/47/turtle-863336_1280.jpg" title="" alt="">
                             <div class="card-body pt-0">
                                 <div class="text-muted mb-4 mt-n3"><span class="bg-primary text-white py-1 px-3 d-inline-block rounded-pill small"><i class="far fa-clock me-2"></i>August 4th 2021</span></div>
                                 <h5>Save the Oceans!</h5>
                                 <p class="pt-3 m-0">With a little bit of code you can do a lot to help to save the oceans. Let's...</p>
                             </div></a>
-                        </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm-hover">
-                            <a href="blog-sense-of-where-you-are.php"><img class="card-img-top" src="static/img/blog/blog-no-img.jpg" title="" alt="">
+                </div>
+                <div class="col-lg-4">
+                    <div class="card shadow-sm-hover">
+                        <a href="blog-sense-of-where-you-are.php"><img class="card-img-top" src="static/img/blog/blog-no-img.jpg" title="" alt="">
                             <div class="card-body pt-0">
                                 <div class="text-muted mb-4 mt-n3"><span class="bg-primary text-white py-1 px-3 d-inline-block rounded-pill small"><i class="far fa-clock me-2"></i>July 30th 2021</span></div>
                                 <h5>A sense of where you are</h5>
                                 <p class="pt-3 m-0">This is based on an article published in the yearly report of enpact.org...</p>
                             </div></a>
-                        </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card shadow-sm-hover">
-                            <a href="blog-new-design.php"><img class="card-img-top" src="static/img/blog/blog-no-img.jpg" title="" alt="">
+                </div>
+                <div class="col-lg-4">
+                    <div class="card shadow-sm-hover">
+                        <a href="blog-new-design.php"><img class="card-img-top" src="static/img/blog/blog-no-img.jpg" title="" alt="">
                             <div class="card-body pt-0">
                                 <div class="text-muted mb-4 mt-n3"><span class="bg-primary text-white py-1 px-3 d-inline-block rounded-pill small"><i class="far fa-clock me-2"></i>July 28th 2021</span></div>
                                 <h5>We have a new design</h5>
                                 <p class="pt-3 m-0">Welcome to our new site! We are excited to announce ...</p>
                             </div></a>
-                        </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
-    </main>
-    <!-- End Main -->
-   <!-- Footer -->
-      <?php
-      include "footer.php";
-      ?>
-      <!-- End Footer -->
+</main>
+<!-- End Main -->
+<!-- Footer -->
+<?php
+include "footer.php";
+?>
+<!-- End Footer -->
 
-    <!-- jquery -->
-    <script src="static/js/jquery-3.5.1.min.js"></script>
-    <!-- appear -->
-    <script src="static/vendor/appear/jquery.appear.js"></script>
-    <!--bootstrap-->
-    <script src="static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- countTo -->
-    <script src="static/vendor/counter/jquery.countTo.js"></script>
-    <!-- typed -->
-    <script src="static/vendor/typed/typed.js"></script>
-    <!-- owl-carousel -->
-    <script src="static/vendor/owl-carousel/js/owl.carousel.min.js"></script>
-    <!-- magnific -->
-    <script src="static/vendor/magnific/jquery.magnific-popup.min.js"></script>
-    <!-- isotope -->
-    <script src="static/vendor/isotope/isotope.pkgd.min.js"></script>
-    <!-- svg-injector -->
-    <script src="static/vendor/svginjector/svg-injector.min.js"></script>
-    <!-- jarallax -->
-    <script src="static/vendor/jarallax/jarallax-all.js"></script>
-    <!-- count-down -->
-    <script src="static/vendor/count-down/jquery.countdown.min.js"></script>
-    <!-- working form -->
-    <script src="static/vendor/mail/js/form.min.js"></script>
-    <script src="static/vendor/mail/js/script.js"></script>
-    <!-- Theme Js -->
-    <script src="static/js/custom.js"></script>
+<!-- jquery -->
+<script src="static/js/jquery-3.5.1.min.js"></script>
+<!-- appear -->
+<script src="static/vendor/appear/jquery.appear.js"></script>
+<!--bootstrap-->
+<script src="static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- countTo -->
+<script src="static/vendor/counter/jquery.countTo.js"></script>
+<!-- typed -->
+<script src="static/vendor/typed/typed.js"></script>
+<!-- owl-carousel -->
+<script src="static/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+<!-- magnific -->
+<script src="static/vendor/magnific/jquery.magnific-popup.min.js"></script>
+<!-- isotope -->
+<script src="static/vendor/isotope/isotope.pkgd.min.js"></script>
+<!-- svg-injector -->
+<script src="static/vendor/svginjector/svg-injector.min.js"></script>
+<!-- jarallax -->
+<script src="static/vendor/jarallax/jarallax-all.js"></script>
+<!-- count-down -->
+<script src="static/vendor/count-down/jquery.countdown.min.js"></script>
+<!-- working form -->
+<script src="static/vendor/mail/js/form.min.js"></script>
+<script src="static/vendor/mail/js/script.js"></script>
+<!-- Theme Js -->
+<script src="static/js/custom.js"></script>
 </body>
 <!-- end body -->
 
