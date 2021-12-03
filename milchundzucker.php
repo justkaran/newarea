59e6a8012eaca4672f85bf8a30e3bdf306077a9a
justkaran <kdegani@gmail.com>
2021-12-03 20:02:48 +0100

    added milch

---
+++ milchundzucker.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <!-- Title -->
+    <title>CodeDoor - Project Based Learning</title>
+    <!-- Required Meta Tags Always Come First -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- CSS Template -->
+    <link href="static/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Skippy -->
+    <a id="skippy" class="sr-only sr-only-focusable u-skippy" href="#content">
+        <div class="container">
+            <span class="u-skiplink-text">Skip to main content</span>
+        </div>
+    </a>
+    <!-- End Skippy -->
+    <!-- Preload -->
+    <div id="loading" class="preloader">
+        <div class="spinner-border text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- End Preload -->
+    <!-- Header -->
+    <?php
+    include "header.php";
+    ?>
+    <!-- End Header -->
+    <!-- Main -->
+    <main>
+        <!-- section -->
+        <section class="section" style="background:#0D4963">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6 text-center text-white">
+                        <h1>Milch und Zucker Projekt</h1>
+                        <div class="lead">Redi School of Digital Integration und CodeDoor machen Dich fit für eine Stelle bei Milch und Zucker </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End section -->
+        <!-- section -->
+        <section class="section">
+            <div class="container">
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-lg-6 my-1">
+                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                        <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_y55svid8.json"  background="transparent"  speed="1"  style="width: 500px; height: 500px; padding-left: 100px; padding-bottom: 100px;"  loop  autoplay></lottie-player>
+                    </div>
+                    <div class="col-lg-6 my-3 ps-lg-8">
+                        <h6 class="pb-2 text-primary">Die Job Placements</h6>
+                        <h3>Du kannst aus drei Kategorien wählen. Für jede Kategorie gibt es Projekte.</h3>
+                        <p class="lead">Die Projekte sind von Milch und Zucker abgesegnet. Wenn Du zeigst, dass Du sie beherrschst, hast Du eine gute Chance auf ein Job Placement.</p>
+                            <br>
+                            <br>
+                            <strong>Die Projektkategorien</strong>
+                        <ul class="list-unstyled m-0 mt-5">
+                            <li class="d-flex mb-4">
+                                <div class="icon icon-sm icon-primary-light rounded-circle">
+                                    <i class="bi bi-check-all fs-5 lh-1"></i>
+                                </div>
+                                <div class="col ps-3">
+                                    <h6 class="mb-1">PHP Projekte</h6>
+                                    <p class="m-0">PHP Projekte machen Dich fit für eine Stelle bei Milch und Zucker.</p>
+                                </div>
+                            </li>
+                            <li class="d-flex mb-4">
+                                <div class="icon icon-sm icon-warning-light rounded-circle">
+                                    <i class="bi bi-check-all fs-5 lh-1"></i>
+                                </div>
+                                <div class="col ps-3">
+                                    <h6 class="mb-1">Communication Manager (Sales, Accounts, Trainer)</h6>
+                                    <p class="m-0">Ob Trainer, Sales oder Accounts Manager. Du kannst aus drei Kategorien wählen. Für jede Kategorie gibt es Projekte.</p>
+                                </div>
+                            </li>
+                            <li class="d-flex mb-4">
+                                <div class="icon icon-sm icon-danger-light rounded-circle">
+                                    <i class="bi bi-check-all fs-5 lh-1"></i>
+                                </div>
+                                <div class="col ps-3">
+                                    <h6 class="mb-1">Ausbildung IT-Bereich</h6>
+                                    <p class="m-0">Hier zeigst Du an Projekten, Deine Eigeninitiative, Neugierde und den Willen, zu lernen.</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End section -->
+<!-- Section -->
+        <section class="section bg-gray-100">
+            <div class="container" id="contact">
+                <div class="row g-3 align-items-center flex-row-reverse">
+                    <div class="col-lg-6 ps-xxl-12">
+                        <h5 class="display-6 mb-3">Contact us</h5>
+                        <p class="lead">Our Senior Account Manager Anna Helfrich is happy to help.</p>
+
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="effect-section">
+                            <div class="svg-effect-3">
+                                <img class="svg_img text-primary" src="static/img/effect/effect-style-1.svg" title="" alt="">
+                            </div>
+                            <div class="svg-effect-4">
+                                <img class="svg_img text-success" src="static/img/effect/effect-style-1.svg" title="" alt="">
+                            </div>
+                            <div class="position-relative px-8">
+                                <img src="static/img/anna.jpg" title="" alt="">
+                            </div>
+                            <div class="card position-absolute bottom-0 end-0 col-8 col-sm-8 mb-4">
+                                <div class="card-body d-flex p-3">
+                                    <div class="icon icon-primary-light rounded-circle mb-4">
+                                        <a href="mailto:getstarted@codedoor.org?subject=Get Started CodeDoor&body=Hi Anna, I would like to learn more about CodeDoor and how to get started. Kind Regards"> <i class="bi bi-mailbox"></i></a>
+                                    </div>
+                                    <div class="col ps-3">
+                                        <h6>Anna Helfrich</h6>
+                                        <p class="m-0">Senior Account Manager</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+  <!-- End section -->
+        <!-- section -->
+        <!-- End section -->
+    </main>
+    <!-- End Main -->
+      <!-- Footer -->
+      <?php
+    include "footer.php";
+    ?>
+    <!-- End Footer -->
+    <!-- jquery -->
+    <script src="static/js/jquery-3.5.1.min.js"></script>
+    <!-- appear -->
+    <script src="static/vendor/appear/jquery.appear.js"></script>
+    <!--bootstrap-->
+    <script src="static/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- countTo -->
+    <script src="static/vendor/counter/jquery.countTo.js"></script>
+    <!-- typed -->
+    <script src="static/vendor/typed/typed.js"></script>
+    <!-- owl-carousel -->
+    <script src="static/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+    <!-- magnific -->
+    <script src="static/vendor/magnific/jquery.magnific-popup.min.js"></script>
+    <!-- isotope -->
+    <script src="static/vendor/isotope/isotope.pkgd.min.js"></script>
+    <!-- svg-injector -->
+    <script src="static/vendor/svginjector/svg-injector.min.js"></script>
+    <!-- jarallax -->
+    <script src="static/vendor/jarallax/jarallax-all.js"></script>
+    <!-- count-down -->
+    <script src="static/vendor/count-down/jquery.countdown.min.js"></script>
+    <!-- working form -->
+    <script src="static/vendor/mail/js/form.min.js"></script>
+    <script src="static/vendor/mail/js/script.js"></script>
+    <!-- Theme Js -->
+    <script src="static/js/custom.js"></script>
+</body>
+<!-- end body -->
+
+</html>
